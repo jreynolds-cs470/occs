a8e095ff5704b2560c66068703769ac13f4cffb1
Joshua Reynolds <reyno28@email.lcc.edu>
2013-10-15 09:48:26 -0400

    fixed app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return redirect(url_for('static', filename='homepage1.html'))
+    return redirect(url_for('static', filename='homework1.html'))
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
